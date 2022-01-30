bc4386f5302f67188d07dc511c9a327181c0ac7a
Codeprince <anyaohaprince122@gmail.com>
2022-01-30 08:35:50 +0100

    added Map.jsx

---
+++ src/components/Map.jsx
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+
+import GoogleMapReact from "google-map-react";
+import Container from "react-bootstrap/Container";
+import { getAddress, formatAddress } from "../utils";
+import "../styles/map.css";
