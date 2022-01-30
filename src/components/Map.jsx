384d5da6cb328486f5d5120379f154b0faa6fe71
Codeprince <anyaohaprince122@gmail.com>
2022-01-30 08:37:01 +0100

    added google map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,3 +4,29 @@ import GoogleMapReact from "google-map-react";
 import Container from "react-bootstrap/Container";
 import { getAddress, formatAddress } from "../utils";
 import "../styles/map.css";
+
+const defaultCenter = {
+  lat: 9.083333333,
+  lng: 7.533333,
+};
+
+let infoWindow;
+let marker;
+
+const Map = () => {
+  return (
+    <Container className="map-wrapper">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GMAP_API_KEY }}
+        onGoogleApiLoaded={addFindMeButton}
+        yesIWantToUseGoogleMapApiInternals
+        defaultCenter={defaultCenter}
+        center={center}
+        defaultZoom={6}
+        zoom={zoom}
+      />
+    </Container>
+  );
+};
+
+export default Map;
