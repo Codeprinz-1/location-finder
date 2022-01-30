519a0b3bde0d38445b945640bac7cfb72ddf8c9e
Codeprince <anyaohaprince122@gmail.com>
2022-01-30 08:39:24 +0100

    added showLocationOnMap function

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -17,6 +17,38 @@ const Map = () => {
   const [zoom, setZoom] = useState(6);
   const [center, setCenter] = useState(defaultCenter);
 
+  const showLocationOnMap = (map, maps, coordinates) => {
+    getAddress(coordinates.lat, coordinates.lng).then((address) => {
+      const addressCard = document.createElement("div");
+      const formattedAddress = formatAddress(address);
+      // checks if an infoWindow or marker is open and closes it to avoid duplecate info windows or markers on the map
+      marker && marker.setMap(null);
+      infoWindow && infoWindow.close();
+      addressCard.classList.add("address-card");
+      addressCard.innerText = `Hey we found you, you are at ${formattedAddress} 🙂`;
+      marker = new maps.Marker({
+        position: coordinates,
+        map,
+      });
+      infoWindow = new maps.InfoWindow();
+      infoWindow.setContent(addressCard);
+      marker.addListener("click", () => {
+        infoWindow.open({
+          anchor: marker,
+          map,
+          shouldFocus: true,
+        });
+      });
+      infoWindow.open({
+        anchor: marker,
+        map,
+        shouldFocus: true,
+      });
+      setZoom(16);
+      setCenter(coordinates);
+    });
+  };
+
   return (
     <Container className="map-wrapper">
       <GoogleMapReact
