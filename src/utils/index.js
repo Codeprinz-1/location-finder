6ac276f543536d040bd8108b97c8960f58f18604
Codeprince <anyaohaprince122@gmail.com>
2022-01-30 08:30:02 +0100

    added getAddress utility function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,9 @@
 import axios from "axios";
+
+// api request function
+export const getAddress = async (latitude, longitude) => {
+  const response = await axios.get(
+    `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${process.env.REACT_APP_OCAGE_API_KEY}`
+  );
+  return response.data.results[0].formatted;
+};
